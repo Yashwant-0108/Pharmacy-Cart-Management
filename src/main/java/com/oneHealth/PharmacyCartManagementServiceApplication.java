5b20d67cac1e0e474a903bfd113b0866f611b6b3
Yashwant <yashrathore5598@gmail.com>
2023-09-12 10:57:31 +0530

    Cart Updated

--- src/main/java/com/oneHealth/PharmacyCartManagementServiceApplication.java
+++ src/main/java/com/oneHealth/PharmacyCartManagementServiceApplication.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class PharmacyCartManagementServiceApplication {
@@ -11,6 +12,14 @@ public class PharmacyCartManagementServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PharmacyCartManagementServiceApplication.class, args);
 	}
+	@GetMapping
+    public String Welcome() {
+
+        // This method handles GET requests to the root URL and returns a welcome message.
+
+        return "Welcome From OneHealth Team (Pharmacy Cart Management Service)!!!";
+
+    }
 
 	@Bean
 	public ModelMapper modelMapper() {
